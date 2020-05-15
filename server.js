a2a2448057945ea66a9935a4d3f9dad27ef53c1a
Tóth Bálint <h776628@stud.u-szeged.hu>
2020-05-15 15:18:01 +0200

    asd

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.disable("x-powered-by");
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+app.use(express.static(__dirname + '/dist/online-arcade'));
+app.get('/*', (req,res) => {
+    res.sendFile(path.join(__dirname + '/dist/online-arcade/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
